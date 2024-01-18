b486ba208614d3701b24034b9b4f37326c274304
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-01-18 10:31:04 -0600

    Initial sprinngboot setup commit

---
+++ src/main/java/com/yshrestha/cs545/restful/Cs545Application.java
@@ -0,0 +1,13 @@
+package com.yshrestha.cs545.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs545Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs545Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yshrestha/cs545/restful/Cs545ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yshrestha.cs545.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs545ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
